dc90d089405625c5fbd0b4c5d248b9bf9240dfc7
Yoann Bigor <bigor44@gmail.com>
2025-10-28 15:28:10 +0100

    nvim config

--- home/nvim.nix
+++ home/nvim.nix
@@ -8,19 +8,13 @@
     vimAlias = true;
     vimdiffAlias = true;
     plugins = with pkgs.vimPlugins; [
-      nvim-lspconfig
-      vim-nix
-      gruvbox
-      luasnip
-      nvim-cmp
-      cmp_luasnip
-      cmp-nvim-lsp
-      fidget-nvim
+      nvim-treesitter.withAllGrammars
+      catppuccin-nvim
     ];
     extraConfig = ''
       set number relativenumber
       set mouse=a
-      colorscheme gruvbox
+      colorscheme catppuccin
     '';
   };
 }
