a22a7779bef82bb2218e3f27994c4b88302889a5
Yoann Bigor <bigor44@gmail.com>
2025-10-31 19:07:53 +0100

    je teste

--- home.nix
+++ home.nix
@@ -2,6 +2,9 @@
   home.username = "bigor";
   home.homeDirectory = "/home/bigor";
   home.stateVersion = "25.11";
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
   home.packages = with pkgs; [
   ];
   imports = [
