4c90bab2951e3dd095d187d5025f3622ab88c3dc
Yoann Bigor <bigor44@gmail.com>
2025-11-02 05:23:37 +0100

    HM in one file

--- home.nix
+++ home.nix
@@ -4,12 +4,47 @@
   home.stateVersion = "25.11";
   home.sessionVariables = {
     EDITOR = "nvim";
-    VISUAL = "nvim";
   };
   home.packages = with pkgs; [
   ];
-  imports = [
-    ./home/git.nix
-    ./home/shell.nix
-  ];
+  programs.git = {
+    enable = true;
+    settings.user = {
+      name = "Yoann Bigor";
+      email = "bigor44@gmail.com";
+    };
+  };
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ll = "exa -l";
+      la = "exa -lah";
+      edit = "sudo -e";
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos";
+      nrb = "sudo nixos-rebuild boot --flake ~/nixos";
+      nfu = "nix flake update";
+      nfc = "nix flake check";
+    };
+  };
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ll = "exa -l";
+      la = "exa -lah";
+      edit = "sudo -e";
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos";
+      nrb = "sudo nixos-rebuild boot --flake ~/nixos";
+      nfu = "nix flake update";
+      nfc = "nix flake check";
+    };
+    shellAbbrs = {
+      gaa = "git add -A";
+      gc = "git commit";
+      gcm = "git commit -m";
+      gd = "git diff";
+      gl = "git pull";
+      gp = "git push";
+      gst = "git status";
+    };
+  };
 }
--- home/git.nix
+++
@@ -1,9 +0,0 @@
-{...}: {
-  programs.git = {
-    enable = true;
-    settings.user = {
-      name = "Yoann Bigor";
-      email = "bigor44@gmail.com";
-    };
-  };
-}
