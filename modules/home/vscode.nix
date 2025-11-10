7a20b5833b2b9be00c9822f1dfffce13f76afd8f
Yoann Bigor <bigor44@gmail.com>
2025-11-10 18:54:41 +0100

    refactor(vscode): Update deprecated vscode options
    Updated programs.vscode.extensions and programs.vscode.userSettings to
    programs.vscode.profiles.default.extensions and
    programs.vscode.profiles.default.userSettings respectively,
    addressing deprecation warnings.

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -9,6 +9,28 @@ in
   config = mkIf cfg.enable {
     programs.vscode = {
       enable = true;
+      profiles.default.extensions = with pkgs.vscode-extensions; [
+        # Nix
+        jnoortheen.nix-ide
+
+        # Bash
+        mads-hartmann.bash-ide-vscode
+
+        # Python
+        ms-python.python
+        ms-python.vscode-pylance
+
+        # Markdown
+        yzhang.markdown-all-in-one
+      ];
+      profiles.default.userSettings = {
+        "[nix]" = {
+          "editor.insertSpaces" = true;
+          "editor.tabSize" = 2;
+        };
+        "python.languageServer" = "Pylance";
+        "markdown.preview.breaks" = true;
+      };
     };
   };
 }
