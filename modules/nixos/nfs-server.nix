6f9d9861f411f6ee248d651dcfe62bfcfeb126dc
Yoann Bigor <bigor44@gmail.com>
2025-11-11 18:08:04 +0100

    nfs server fixes

--- modules/nixos/nfs-server.nix
+++ modules/nixos/nfs-server.nix
@@ -7,7 +7,7 @@ lib.mkIf config.nfs.server.enable {
   services.nfs.server = {
     enable = true;
     exports = ''
-      /mnt/storage 192.168.1.1(rw,sync,no_subtree_check,no_root_squash)
+      /mnt/storage 192.168.1.1(rw,sync,no_subtree_check,all_squash,anonuid=1000,anongid=100)
     '';
   };
 
@@ -15,4 +15,9 @@ lib.mkIf config.nfs.server.enable {
     allowedTCPPorts = [ 2049 ]; # NFS
     allowedUDPPorts = [ 2049 ];
   };
+
+  # Ensure correct permissions on /mnt/storage
+  systemd.tmpfiles.rules = [
+    "d /mnt/storage 0755 bigor users -"
+  ];
 }
