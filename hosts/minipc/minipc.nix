5a2b5763d5e4b9124a6f4dc6e280f03fc248d40f
Yoann Bigor <bigor44@gmail.com>
2025-11-14 11:03:00 +0100

    opti

--- hosts/minipc/minipc.nix
+++ hosts/minipc/minipc.nix
@@ -4,4 +4,11 @@
   server.enable = true;
   adblocker.enable = true;
   nfs.server.enable = true;
+
+  boot.kernelParams = [ 
+    "amd_pstate=active"
+    "processor.max_cstate=1"  # Better for 24/7 server
+  ];
+  powerManagement.cpuFreqGovernor = "schedutil";
+  hardware.cpu.amd.updateMicrocode = true;
 }
