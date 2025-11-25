cea0803670e3dd9e4692fdc08f9d715020388be0
Yoann Bigor <bigor44@gmail.com>
2025-11-25 21:23:41 +0100

    Reorg Dotfiles

--- modules/home/dotfiles.nix
+++ modules/home/dotfiles.nix
@@ -1,12 +1,8 @@
 { config, ... }:
-let
-  # Ajustez ce chemin selon l'endroit où vous clonez votre repo sur le disque
-  dotfilesPath = "${config.home.homeDirectory}/nixos/dotfiles";
-in
 {
   xdg.configFile = {
-    "nvim".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/nvim";
-    "cosmic".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/cosmic";
-    "autostart".source = config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/autostart";
+    "nvim".source = config.lib.file.mkOutOfStoreSymlink "./dotfile/nvim";
+    "cosmic".source = config.lib.file.mkOutOfStoreSymlink "./dotfiles/cosmic";
+    "autostart".source = config.lib.file.mkOutOfStoreSymlink "./dotfiles/autostart";
   };
 }
