ca3cfb5928915ba93d294b2fbe7491b74c596e16
Yoann Bigor <bigor44@gmail.com>
2025-11-29 17:17:37 +0100

    build: deduplicate flake-parts in flake.lock

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
     };
 
     pre-commit-hooks = {
