478987ecbe3c170a120b42cfe190ed604de77ad0
Yoann Bigor <bigor44@gmail.com>
2025-12-02 00:43:51 +0100

    reset nvim

--- dotfiles/nvim/init.lua
+++
@@ -1,8 +0,0 @@
----
--- Entry point for Neovim configuration.
--- Orchestrates the loading of options, keymaps, and the package manager.
----
-
-require("config.options")
-require("config.keymaps")
-require("config.lazy")
--- dotfiles/nvim/lua/plugins/editor.lua
+++
@@ -1,5 +0,0 @@
----
--- Editor Plugins.
--- All editor functionality has been migrated to mini.nvim modules in mini.lua.
----
-return {}
