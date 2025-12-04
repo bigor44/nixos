bafeac79fe4561fe4684a2272322d8e143b6b568
Yoann Bigor <bigor44@gmail.com>
2025-12-04 15:34:52 +0100

    gestion des dotfiles

--- modules/home/dotfiles.nix
+++ modules/home/dotfiles.nix
@@ -2,7 +2,7 @@
 {
   xdg.configFile = {
     cosmic.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/dotfiles/cosmic";
-    autostart.source = "../../dotfiles/autostart";
+    autostart.source = ../../dotfiles/autostart;
     nvim.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/dotfiles/nvim";
   };
 }
