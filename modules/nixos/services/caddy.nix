2ce14a71b2efb2ea85e32acd9f18470248d87566
Yoann Bigor <bigor44@gmail.com>
2025-12-08 19:01:43 +0100

    fix(services): re-add caddy firewall rules

--- modules/nixos/services/caddy.nix
+++ modules/nixos/services/caddy.nix
@@ -6,33 +6,6 @@
 lib.mkIf config.roles.homelab_master {
   services.caddy = {
     enable = true;
-
-    # Virtual Hosts Configuration
-    virtualHosts = {
-      # Dashboard (Homepage)
-      "home.bigor.lan" = {
-        extraConfig = ''
-          reverse_proxy 127.0.0.1:8082
-          tls internal # Use Caddy's internal CA for local HTTPS
-        '';
-      };
-
-      # AdGuard Home (DNS Admin)
-      "adguard.bigor.lan" = {
-        extraConfig = ''
-          reverse_proxy 127.0.0.1:3003
-          tls internal
-        '';
-      };
-
-      # Glances (System Monitoring)
-      "glances.bigor.lan" = {
-        extraConfig = ''
-          reverse_proxy 127.0.0.1:61208
-          tls internal
-        '';
-      };
-    };
   };
 
   networking.firewall.allowedTCPPorts = [
