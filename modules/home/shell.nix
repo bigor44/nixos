89e5f9df20be3950a2b3aebbf0faf3e25b53519d
Yoann Bigor <bigor44@gmail.com>
2025-12-09 03:23:13 +0100

    refactor(home/shell): remove unused gai function and interactive init

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -58,47 +58,6 @@
         meminfo = "free -h";
         diskinfo = "df -h";
       };
-
-      functions = {
-        gai = ''
-          set diff (git diff --cached)
-          if test -z "$diff"
-              echo "No staged changes to commit."
-              return 1
-          end
-
-          echo "Generating commit message..."
-          set msg (echo "$diff" | gemini "Generate a concise, conventional commit message for this diff. Return ONLY the raw message text, no markdown, no quotes.")
-
-          if test -z "$msg"
-              echo "Failed to generate message."
-              return 1
-          end
-
-          echo "----------------------------------------------------------------"
-          echo "$msg"
-          echo "----------------------------------------------------------------"
-
-          read -l -P "Commit with this message? [y/N] " confirm
-          if test "$confirm" = "y"
-              git commit -m "$msg"
-          else
-              echo "Commit cancelled."
-          end
-        '';
-      };
-
-      interactiveShellInit = ''
-        if test -f ${pkgs.nh}/share/fish/vendor_conf.d/nh.fish
-          source ${pkgs.nh}/share/fish/vendor_conf.d/nh.fish
-        end
-
-        source ${pkgs.nix}/share/fish/vendor_completions.d/nix.fish
-
-        set fish_greeting
-
-        set -gx EZA_COLORS "da=38;5;240:gm=38;5;240"
-      '';
     };
 
     fzf = {
