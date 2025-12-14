7aabde54525f9392fb77d11fe5e4e022717c213b
Yoann Bigor <bigor44@gmail.com>
2025-12-14 05:19:16 +0100

    25.11 => Unstable

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   # - nixvim: Neovim configuration via Nix
   # ============================================================================
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     snowfall-lib = {
       url = "github:snowfallorg/lib";
@@ -19,12 +19,12 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-25.11";
+      url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
