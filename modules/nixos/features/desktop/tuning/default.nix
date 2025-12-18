7cfa11bfb7448e877dd77f4b0f5ecd60ae93f2f6
Yoann Bigor <bigor44@gmail.com>
2025-12-18 17:23:44 +0100

    silent boot

--- modules/nixos/features/desktop/tuning/default.nix
+++ modules/nixos/features/desktop/tuning/default.nix
@@ -20,6 +20,10 @@ in
       kernelParams = [
         "quiet"
         "splash"
+        "rd.systemd.show_status=false"
+        "rd.udev.log_level=3"
+        "udev.log_priority=3"
+        "boot.shell_on_fail"
       ];
       plymouth = {
         enable = true;
