532d0fe3728d3d0c8f322ea3eee345be5071fa52
Yoann Bigor <bigor44@gmail.com>
2025-12-22 23:11:10 +0100

    cleanup ollama module

--- modules/nixos/services/ollama/default.nix
+++ modules/nixos/services/ollama/default.nix
@@ -14,17 +14,6 @@ in
   options.bigor.services.ollama.enable = mkEnableOption "Ollama LLM service";
 
   config = mkIf cfg.enable {
-    # Register Ollama in registry
-    bigor.registry.services.ollama = {
-      inherit (config.networking) hostName;
-      port = 8080;
-      domain = "ai.bigor.lan";
-      reverseProxy = true;
-      openFirewall = false;
-      openFirewallUDP = false;
-      proxyProtocol = "http";
-    };
-
     services.ollama = {
       enable = true;
       host = "127.0.0.1";
