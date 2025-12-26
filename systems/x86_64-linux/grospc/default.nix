d7e9affcaf02aa40787829efc61fe58e0060c3d6
Yoann Bigor <bigor44@gmail.com>
2025-12-26 19:21:58 +0100

    Conf wallpaper && fin réinstall grospc (steamlibrary)

--- systems/x86_64-linux/grospc/default.nix
+++ systems/x86_64-linux/grospc/default.nix
@@ -23,4 +23,13 @@
       upstreamHost = "minipc";
     };
   };
+
+  fileSystems."/steamlibrary" = {
+    device = "/dev/disk/by-uuid/eca2097b-72d2-46cc-95d3-3b1d546afffc";
+    fsType = "ext4";
+    options = [
+      "noatime"
+      "nodiratime"
+    ]; # Reduce write wear
+  };
 }
