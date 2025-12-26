caa86415fd6443fb0d9494a0958d6cb7f7445ea6
Yoann Bigor <bigor44@gmail.com>
2025-12-26 18:40:46 +0100

    reinstall grospc

--- systems/x86_64-linux/grospc/default.nix
+++ systems/x86_64-linux/grospc/default.nix
@@ -23,13 +23,4 @@
       upstreamHost = "minipc";
     };
   };
-
-  fileSystems."/steamlibrary" = {
-    device = "/dev/disk/by-uuid/84c2f17e-37c6-4ef9-b98c-6862c808990b";
-    fsType = "ext4";
-    options = [
-      "noatime"
-      "nodiratime"
-    ]; # Reduce write wear
-  };
 }
