365b1df2091657d7352b56dc6ba2b78b077ffc7b
Yoann Bigor <bigor44@gmail.com>
2026-01-01 16:25:34 +0100

    formatting on save

--- modules/home/nixvim/autocmds.nix
+++ modules/home/nixvim/autocmds.nix
@@ -25,6 +25,17 @@ _: {
           __raw = "function() vim.opt_local.formatoptions:remove({ 'r', 'o' }) end";
         };
       }
+      # Format on save using LSP
+      {
+        event = "BufWritePre";
+        callback = {
+          __raw = ''
+            function()
+              vim.lsp.buf.format({ async = false })
+            end
+          '';
+        };
+      }
     ];
   };
 }
