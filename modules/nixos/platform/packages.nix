8c3799aae705f72dfc5c3b9e6421273a10265746
Yoann Bigor <bigor44@gmail.com>
2026-01-25 11:08:51 +0100

    fix

--- modules/nixos/platform/packages.nix
+++ modules/nixos/platform/packages.nix
@@ -7,8 +7,6 @@
     tmux.enable = true;
     nh = {
       enable = true;
-      clean.enable = true;
-      clean.extraArgs = "--keep-since 4d --keep 3";
       flake = "/home/bigor/nixos";
     };
   };
